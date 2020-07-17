423475f1b46d3e3a2713061cdc67cd2c645f7d05
“shrawanamit” <amitkumar06111@gmail.com>
2020-07-17 07:14:19 +0530

    added dash board of employee detail

--- src/Components/Employee/DashBoard.jsx
+++ src/Components/Employee/DashBoard.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';  
+import Paper from '@material-ui/core/Paper';
+import TableCell from '@material-ui/core/TableCell';  
+import TableRow from '@material-ui/core/TableRow'; 
+import TableHead from '@material-ui/core/TableHead';  
+import TableContainer from '@material-ui/core/TableContainer';  
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import {Link } from "react-router-dom";
 import "./DashBoard.scss";
 
 export class DashBoard extends React.Component {
@@ -11,38 +18,33 @@ export class DashBoard extends React.Component {
 render() 
     {
       return (
-          <div className="body">
-                     <p class="title" align="center">
-                        <Typography component="h3" variant="h5">
-                            Employee DashBoard
-                        </Typography>
-                    </p>
-                    <div className="button" align="center">
-                        <Button variant="contained" color="primary">
-                            Add Employee
-                        </Button>
-                    </div>
-                    <div className="button" align="center">
-                        <Button variant="contained" color="primary">
-                            Update Employee
-                        </Button>
-                    </div>
-                    <div className="button" align="center">
-                        <Button variant="contained" color="primary">
-                            Delete Employee
-                        </Button>
-                    </div>
-                    <div className="button" align="center">
-                        <Button variant="contained" color="primary">
-                            Get All Employee
-                        </Button>
-                    </div>
-                    <div className="button"align="center">
-                        <Button variant="contained" color="primary">
-                            Get Employee by Id
-                        </Button>
-                    </div>
-            </div>
+        <TableContainer component={Paper}> 
+        <div className="employeebtn">
+        <Link to="/">
+        <Button variant="contained" color="primary"> Add Employee </Button>
+        </Link>
+        </div>
+        <p class="title" align="center">
+        <Typography component="h1" variant="h5"> <b>Employee Details</b> </Typography>
+        </p>
+          <Table stickyHeader aria-label="sticky table" id='Employee'>  
+            <TableHead>  
+              <TableRow>  
+                <TableCell>ID</TableCell>  
+                <TableCell align="right" >First Name</TableCell>  
+                <TableCell align="right" >Last Name</TableCell>  
+                <TableCell align="right" >MobileNo</TableCell>  
+                <TableCell align="right" >Email Id</TableCell>  
+                <TableCell align="right" >City</TableCell>  
+                <TableCell align="right" >Department</TableCell>  
+                <TableCell align="right" >Salary</TableCell>
+                <TableCell align="right" >Update</TableCell> 
+                <TableCell align="right" >Delete</TableCell>
+              </TableRow>  
+            </TableHead>  
+  
+          </Table>  
+        </TableContainer>  
       );
     }
 }
